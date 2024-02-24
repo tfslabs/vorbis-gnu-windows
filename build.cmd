ee38464169f79544a86bbf18cd2a4a8f4f5178f8
ねこ 白い <shiroineko.tfs@gmail.com>
2024-02-24 23:51:15 +0700

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -5,6 +5,6 @@ md .bin
 md .build
 
 cd .build
-cmake -G "Unix Makefiles" -DCMAKE_PREFIX_PATH="../.bin" ..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=1 ..
 make -j4
 make DESTDIR="../.bin" install
